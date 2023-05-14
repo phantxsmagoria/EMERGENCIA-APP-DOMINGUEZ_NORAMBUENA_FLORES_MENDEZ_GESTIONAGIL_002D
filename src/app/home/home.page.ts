d5c7a76b60a35a3e9d5c910922d6bfcdec2c28e1
phantxsmagoria <nxirmary@gmail.com>
2023-05-14 17:53:56 -0400

    HOME PAGE + FIX BUGS

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,5 +7,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   constructor() {}
+  tweets: any[] = [
+    {
+      username: 'DUOC SEDE MAIPÚ',
+      handle: 'duocucmaipu',
+      like: '1232',
+      retweets: '34k',
+      response: '100',
+      text: '¡¡Emergencia!! Debido a un gran accidente que compromete la calle cinco de abril con primera transversal: Duoc Sede Maipú no abrirá sus puertas el día de hoy.',
+      date: '1581322823',
+      liked: true,
+      retweet: false,
+      attachment: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Duoc_UC_-_Sede_Maip%C3%BA.jpg',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Duoc_UC_-_Sede_Maip%C3%BA.jpg'
+  },
+  {
+    username: 'DUOC SEDE SAN CARLOS DE APOQUINDO',
+    handle: 'duocucsancarlos',
+    like: '25k',
+    retweets: '100k',
+    response: '124',
+    text: '¡¡Suspención de clases debido a corte de agua!! Tras una ruptura de cañería, la sede se encuentra temporalmente cerrada. Por ahora, sólo se darán clases online hasta que el problema se solucione. ¡¡Recordar que las clases online también son de carácter obligatorio!!',
+    date: '1581322823',
+    liked: true,
+    retweet: true,
+    attachment: 'https://live.staticflickr.com/8454/8052574696_2106b56753_b.jpg',
+    img: 'https://live.staticflickr.com/8454/8052574696_2106b56753_b.jpg'
+},
+
+    // Agrega más tweets aquí...
+  ];
 }
 
